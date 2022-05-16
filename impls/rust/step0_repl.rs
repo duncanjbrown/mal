94aee02eb15e53792986e5407c81e233671cdd02
Duncan Brown <duncanjbrown@gmail.com>
2022-05-16 20:51:29 +0100

    Attempt rust

--- impls/rust/step0_repl.rs
+++ impls/rust/step0_repl.rs
@@ -1,31 +1,39 @@
-extern crate rustyline;
-
-use rustyline::error::ReadlineError;
-use rustyline::Editor;
+use std::io;
+use std::io::Write;
+use std::str;
 
 fn main() {
-    // `()` can be used when no completer is required
-    let mut rl = Editor::<()>::new();
-    if rl.load_history(".mal-history").is_err() {
-        eprintln!("No previous history.");
-    }
-
     loop {
-        let readline = rl.readline("user> ");
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(&line);
-                rl.save_history(".mal-history").unwrap();
-                if line.len() > 0 {
-                    println!("{}", line);
+        print!("user> ");
+        io::stdout().flush().expect("Could not flush to stdout");
+
+        let mut input = String::new();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                } else {
+                    rep(&input.trim_end());
                 }
             }
-            Err(ReadlineError::Interrupted) => continue,
-            Err(ReadlineError::Eof) => break,
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
+            Err(error) => println!("Input error! {}", error),
         }
     }
+}
+
+fn rep(line: &str) {
+    print(eval(read(line)))
+}
+
+fn read(line: &str) -> &str {
+    line
+}
+
+fn eval(line: &str) -> &str {
+    line
+}
+
+fn print(line: &str) {
+    println!("{}", line);
 }
